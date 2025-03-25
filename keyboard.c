3d98c9b3cf15cb66c438d2a57c9d82392a289d07
Lily Chen <lilyc3719@gmail.com>
2025-03-25 16:44:21 -0400

    changed pen shape to circle

--- keyboard.c
+++ keyboard.c
@@ -5,6 +5,7 @@
 #define PIXEL_BUF_CTRL_BASE 0xFF203020
 
 #define BOX_SIZE 24
+#define CIRCLE_RADIUS 12
 #define DRAW_COLOR 0xFFFF  // white
 #define BG_COLOR 0x0000    // black
 
@@ -20,6 +21,7 @@ void capture();             // capture the image into an array
 void plot_pixel(int x, int y, short int line_color);
 void clear_screen();
 void draw_box(int x, int y, short int colour);
+void draw_circle(int x_center, int y_center, short int colour);
 
 /* Global Variables */
 int pixel_buffer_start;
@@ -56,71 +58,71 @@ void handle_key(char key) {
   pixel_buffer_start = *pixel_ctrl_ptr;
 
   switch (key) {
-    case 0x75:                      // up arrow
-      *hex = 0b0111110;             // U
-      if (y_cur - BOX_SIZE >= 0) {  // check in bounds
+    case 0x75:                               // up arrow
+      *hex = 0b0111110;                      // U
+      if (y_cur - CIRCLE_RADIUS * 2 >= 0) {  // check in bounds
         short int pixel =
             *(short int *)(pixel_buffer_start + (y_cur << 10) + (x_cur << 1));
         if (pixel == BG_COLOR) {  // only erase cursor if on background
-          draw_box(x_cur, y_cur, BG_COLOR);  // erase cursor
+          draw_circle(x_cur, y_cur, BG_COLOR);  // erase cursor
         }
-        y_cur -= BOX_SIZE / 4;
-        draw_box(x_cur, y_cur, DRAW_COLOR);  // draw cursor
+        y_cur -= CIRCLE_RADIUS * 2 / 4;
+        draw_circle(x_cur, y_cur, DRAW_COLOR);  // draw cursor
       }
 
       if (x_draw != -1 && y_draw != -1 && erase) {  // redundant condition?
-        draw_box(x_draw, y_draw, BG_COLOR);         // erase prev
+        draw_circle(x_draw, y_draw, BG_COLOR);      // erase prev
         erase = false;
       }
       break;
-    case 0x72:                          // down arrow
-      *hex = 0b1011110;                 // d
-      if (y_cur + BOX_SIZE < HEIGHT) {  // check in bounds
+    case 0x72:                                   // down arrow
+      *hex = 0b1011110;                          // d
+      if (y_cur + CIRCLE_RADIUS * 2 < HEIGHT) {  // check in bounds
         short int pixel =
             *(short int *)(pixel_buffer_start + (y_cur << 10) + (x_cur << 1));
         if (pixel == BG_COLOR) {  // only erase cursor if on background
-          draw_box(x_cur, y_cur, BG_COLOR);  // erase cursor
+          draw_circle(x_cur, y_cur, BG_COLOR);  // erase cursor
         }
-        y_cur += BOX_SIZE / 4;
-        draw_box(x_cur, y_cur, DRAW_COLOR);  // draw cursor
+        y_cur += CIRCLE_RADIUS * 2 / 4;
+        draw_circle(x_cur, y_cur, DRAW_COLOR);  // draw cursor
       }
 
       if (x_draw != -1 && y_draw != -1 && erase) {
-        draw_box(x_draw, y_draw, BG_COLOR);  // erase prev
+        draw_circle(x_draw, y_draw, BG_COLOR);  // erase prev
         erase = false;
       }
       break;
-    case 0x6B:                      // left arrow
-      *hex = 0b0111000;             // L
-      if (x_cur - BOX_SIZE >= 0) {  // check in bounds
+    case 0x6B:                               // left arrow
+      *hex = 0b0111000;                      // L
+      if (x_cur - CIRCLE_RADIUS * 2 >= 0) {  // check in bounds
         short int pixel =
             *(short int *)(pixel_buffer_start + (y_cur << 10) + (x_cur << 1));
         if (pixel == BG_COLOR) {  // only erase cursor if on background
-          draw_box(x_cur, y_cur, BG_COLOR);  // erase cursor
+          draw_circle(x_cur, y_cur, BG_COLOR);  // erase cursor
         }
-        x_cur -= BOX_SIZE / 4;
-        draw_box(x_cur, y_cur, DRAW_COLOR);  // draw cursor
+        x_cur -= CIRCLE_RADIUS * 2 / 4;
+        draw_circle(x_cur, y_cur, DRAW_COLOR);  // draw cursor
       }
 
       if (x_draw != -1 && y_draw != -1 && erase) {
-        draw_box(x_draw, y_draw, BG_COLOR);  // erase prev
+        draw_circle(x_draw, y_draw, BG_COLOR);  // erase prev
         erase = false;
       }
       break;
-    case 0x74:                         // right arrow
-      *hex = 0b0110001;                // r
-      if (x_cur + BOX_SIZE < WIDTH) {  // check in bounds
+    case 0x74:                                  // right arrow
+      *hex = 0b0110001;                         // r
+      if (x_cur + CIRCLE_RADIUS * 2 < WIDTH) {  // check in bounds
         short int pixel =
             *(short int *)(pixel_buffer_start + (y_cur << 10) + (x_cur << 1));
         if (pixel == BG_COLOR) {  // only erase cursor if on background
-          draw_box(x_cur, y_cur, BG_COLOR);  // erase cursor
+          draw_circle(x_cur, y_cur, BG_COLOR);  // erase cursor
         }
-        x_cur += BOX_SIZE / 4;
-        draw_box(x_cur, y_cur, DRAW_COLOR);  // draw cursor
+        x_cur += CIRCLE_RADIUS * 2 / 4;
+        draw_circle(x_cur, y_cur, DRAW_COLOR);  // draw cursor
       }
 
       if (x_draw != -1 && y_draw != -1 && erase) {
-        draw_box(x_draw, y_draw, BG_COLOR);  // erase prev
+        draw_circle(x_draw, y_draw, BG_COLOR);  // erase prev
         erase = false;
       }
       break;
@@ -152,7 +154,7 @@ void init_vga() {
   pixel_buffer_start = *pixel_ctrl_ptr;
 
   clear_screen();
-  draw_box(x_cur, y_cur, DRAW_COLOR);  // draw cursor
+  draw_circle(x_cur, y_cur, DRAW_COLOR);  // draw cursor
 }
 
 void capture() {
@@ -193,6 +195,34 @@ void draw_box(int x, int y, short int colour) {
   for (int i = x; i < x + BOX_SIZE; i++) {
     for (int j = y; j < y + BOX_SIZE; j++) {
       plot_pixel(i, j, colour);
+    }
+  }
+}
+
+void draw_circle(int x_center, int y_center, short int colour) {
+  int x = CIRCLE_RADIUS;
+  int y = 0;
+  int decision_over_2 =
+      1 - x;  // decision criterion divided by 2 evaluated at x=r, y=0
+
+  while (y <= x) {
+    // draw horizontal lines between the points on the circle's circumference
+    for (int i = x_center - x; i <= x_center + x; i++) {
+      plot_pixel(i, y_center + y, colour);  // Octant 1 and 4
+      plot_pixel(i, y_center - y, colour);  // Octant 5 and 8
+    }
+    for (int i = x_center - y; i <= x_center + y; i++) {
+      plot_pixel(i, y_center + x, colour);  // Octant 2 and 3
+      plot_pixel(i, y_center - x, colour);  // Octant 6 and 7
+    }
+
+    y++;
+    if (decision_over_2 <= 0) {
+      decision_over_2 +=
+          2 * y + 1;  // change in decision criterion for y -> y+1
+    } else {
+      x--;
+      decision_over_2 += 2 * (y - x) + 1;  // change for y -> y+1, x -> x-1
     }
   }
 }
